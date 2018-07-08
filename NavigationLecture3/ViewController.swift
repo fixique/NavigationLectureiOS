b93bc5321ae78da00ebacb86b469d5192417c25b
Vlad Krupenko <ubeto96@gmail.com>
2018-07-08 21:25:21 +0300

    Initial Commit

---
+++ NavigationLecture3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationLecture3
+//
+//  Created by Vlad Krupenko on 08.07.2018.
+//  Copyright © 2018 Vlad Krupenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
