ebb817ec77890b2fa42899ba68f26e8b5b858095
Vlad Krupenko <dev.fixique@gmail.com>
2018-07-18 01:05:20 +0300

    Add some examples for navigation lecture

--- NavigationLecture3/Screens/MainViewController/MainViewController.swift
+++ NavigationLecture3/Screens/MainViewController/MainViewController.swift
@@ -12,13 +12,34 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
--- NavigationLecture3/Screens/SecondTestController/SecondTestController.swift
+++ NavigationLecture3/Screens/SecondTestController/SecondTestController.swift
@@ -10,26 +10,9 @@ import UIKit
 
 class SecondTestController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    func pushMainSettingsController() {
+        let mainSettingsController = MainSettingsController()
+        self.navigationController?.pushViewController(mainSettingsController, animated: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
